25ed0f6ec0646976afe0b554cc097fb816506092
anselg <aesthapus@gmail.com>
2016-12-15 00:45:35 +0000

    (xenial) avimux muxes raw, uncompressed to avi, so avenc_huffyuv might not be needed

--- src/recorders/gstrecorders/huffyuvavi.cpp
+++ src/recorders/gstrecorders/huffyuvavi.cpp
@@ -27,8 +27,10 @@ HuffyuvAviFormat::makeRecorder(QArvDecoder* decoder, QString fileName,
                                QSize frameSize, int framesPerSecond,
                                bool writeInfo)
 {
+//  return makeGstRecorder({ "avenc_huffyuv", "avimux" },
+//                         "avenc_huffyuv ! avimux", decoder, fileName, frameSize,
   return makeGstRecorder({ "avenc_huffyuv", "avimux" },
-                         "avenc_huffyuv ! avimux", decoder, fileName, frameSize,
+                         "avimux", decoder, fileName, frameSize,
                          framesPerSecond, writeInfo);
 }
 
